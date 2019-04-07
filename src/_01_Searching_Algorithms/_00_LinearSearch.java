6b53fc1751c3f1aed8ffcc3dd660642b9a66fb92
eliyahubowers <eliyahubowers@gmail.com>
2019-04-07 11:30:41 -0700

    Add files via upload

--- src/_01_Searching_Algorithms/_00_LinearSearch.java
+++ src/_01_Searching_Algorithms/_00_LinearSearch.java
@@ -1,5 +1,7 @@
 package _01_Searching_Algorithms;
 
+import java.lang.reflect.Array;
+
 public class _00_LinearSearch {
 	// When finding the location of a certain element in an array, 
 	// there are many algorithms that can do so. The simplest is the
@@ -9,7 +11,13 @@ public class _00_LinearSearch {
 		//   in the array. Return the location in the array
 		//   where the value was found.
 		//   If the value is not found in the array, return -1.
-		
-		return 0;
+		int j = -1;
+		for(int i = 0; i < words.length; i++) {
+			if(value.equals(words[i])) {
+				j = i;
+				i = words.length;
+			}
+		}
+		return j;
 	}
 }
