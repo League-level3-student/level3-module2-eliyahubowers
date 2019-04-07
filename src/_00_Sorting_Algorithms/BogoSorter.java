62b50272bb6836270611341988db8c74ecd4dcd5
eliyahubowers <eliyahubowers@gmail.com>
2019-04-07 11:30:19 -0700

    Add files via upload

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -20,6 +20,26 @@ public class BogoSorter extends Sorter {
 	//1. Complete the sort method using the Bogo sort algorithm. 
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-
+		boolean b = false;
+		Random r = new Random();
+		int one = 0;
+		int two = 0;
+		int swap;
+		while(b == false) {
+			b = true;
+			for(int i = 1; i < array.length; i++) {
+				if(array[i-1] > array[i]) {	
+					b = false;
+				}
+			}
+			if(b != true) {
+				one = r.nextInt(array.length);
+				two = r.nextInt(array.length);
+				swap = array[one];
+				array[one] = array[two];
+				array[two] = swap;
+			}
+			display.updateDisplay();
+		}
 	}
 }
